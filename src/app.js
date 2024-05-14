bfa156d9ed296efd935c4e29d888f79e2b9c6667
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-05-14 17:24:40 +0000

    Project completed

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseP = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  excuseP.innerHTML += generateExcuse();
+
+  function generateExcuse() {
+    let randomWhoIndex = Math.floor(Math.random() * who.length);
+    let randomActionIndex = Math.floor(Math.random() * action.length);
+    let randomWhatIndex = Math.floor(Math.random() * what.length);
+    let randomWhenIndex = Math.floor(Math.random() * when.length);
+
+    return `${who[randomWhoIndex]} ${action[randomActionIndex]} ${what[randomWhatIndex]} ${when[randomWhenIndex]}`;
+  }
 };
